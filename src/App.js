841b63f37aa3fbb5e10c3c6f6387ba0de102a1cf
rohit jaiswal <rohitjaiswal9888rohit@gmail.com>
2023-09-20 17:29:27 +0530

    updated App.js

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import Register from './Components/common/Register'
 import Taskboard from './Pages/Taskboard/Taskboard'
 import Calculator from './Pages/Calculator/calculator'
 import Weather from './Pages/Weather/Weather'
-import Homepage from './Components/common/Homepage';
+
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Register/>} />
         <Route path='/login' element={<Login/>} />
-        <Route path='/homepage' element={<Homepage/>} />
         <Route path='/taskboard' element={<Taskboard/>} />
         <Route path='/calculator' element={<Calculator/>} />
         <Route path='/weather' element={<Weather/>} />        
