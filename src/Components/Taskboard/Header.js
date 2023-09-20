3c2dfbf25858e30d68f70eb52ba4fb298bc6a6f9
rohit jaiswal <rohitjaiswal9888rohit@gmail.com>
2023-09-20 16:40:34 +0530

    updated taskboard ui

---
+++ src/Components/Taskboard/Header.js
@@ -0,0 +1,14 @@
+
+const Header = () => {
+
+    return (
+        <div className="navbar">
+      <div className="navbar-logo">TASK BOARD</div>
+      <div className="profile-photo">
+        <img src="https://picsum.photos/id/77/1631/1102" alt="Profile" />
+      </div>
+    </div>
+    )
+}
+
+export default Header;
--- src/Pages/Taskboard/Taskboard.js
+++ src/Pages/Taskboard/Taskboard.js
@@ -1,11 +1,108 @@
-import React from 'react'
+import "./Taskboard.css"  
+import { BsAlignMiddle } from "react-icons/bs";
+import ListItem from "../../Components/Taskboard/ListItem";
+import { useEffect, useState } from "react";
+import Header from "../../Components/Taskboard/Header";
+const App = () => {
 
-const Taskboard = () => {
+    const [todo, setTodo] = useState("")
+    const [allTodos, setAllTodos] = useState([])
+
+    const addTodo = (e) => {
+        e.preventDefault()
+
+        const todoItem = {
+            id: new Date().getTime(),
+            text: todo,
+            isChecked: false
+        }
+
+        if(todo !== ""){
+            setAllTodos([...allTodos].concat(todoItem).reverse())
+            setTodo("")
+        }
+
+        console.log(allTodos)
+    }
+
+    
+
+    const getAllTodos = () => {
+        let stored = JSON.parse(localStorage.getItem("todo"))
+
+        if(stored) {
+            setAllTodos(stored)
+        }
+    }
+
+
+    const toggleChecked = (id) => {
+        let updatedTodos = [...allTodos].map(todo => {
+            if(todo.id === id){
+                todo.isChecked = !todo.isChecked
+            }
+
+            return todo
+        })
+
+        setAllTodos(updatedTodos)
+    }
+
+
+
+    const deleteTodo = (id) => {
+        const filteredTodo = allTodos.filter(todo => todo.id !== id)
+        setAllTodos(filteredTodo)
+    }
+
+
+    useEffect(() => {
+        getAllTodos()
+    }, [])
+
+
+    useEffect(() => {
+        localStorage.setItem("todo", JSON.stringify(allTodos))
+    }, [allTodos])
+    
+
   return (
-    <div>
-      Taskboard
+    
+    <div className="container">
+        <Header></Header>
+        <div className="App_todo ">
+            {/* <form className="App_input_wrapper" onSubmit={addTodo} >
+                <input type={"text"} className="App_input" value={todo} onChange={(e) => setTodo(e.target.value)} />
+                <div className="App_input_button" onClick={addTodo} >
+                    <BsAlignMiddle size={24} />
+                </div>
+            </form> */}
+
+            <div className="App_todo_list layout">
+           <div className="heading">My tasks!</div>
+
+            <form className="App_input_wrapper" onSubmit={addTodo} >
+                <input type={"text"} className="App_input" value={todo} onChange={(e) => setTodo(e.target.value)} />
+                <div className="App_input_button" onClick={addTodo} >
+                    <BsAlignMiddle size={24} />
+                </div>
+            </form>
+                {
+                    allTodos.map(todo => (
+                        <ListItem key={todo.id} deleteTodo={() => deleteTodo(todo.id)} text={todo.text} isChecked={todo.isChecked} toggleChecked={() => toggleChecked(todo.id)} />
+                    ))
+                }
+
+                {
+                    allTodos.length === 0 && (
+                        <p className="empty"></p>
+                    )
+                }
+            </div>
+        </div>
     </div>
+    
   )
 }
 
-export default Taskboard
+export default App
